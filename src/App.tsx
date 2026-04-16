e0958620eb2a6b1f2e12b37835ba17e07b502003
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-04-16 20:02:31 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <button className="btn btn-primary">Hola Mundo</button>
+    </div>
+  );
+}
+
+export default App;
